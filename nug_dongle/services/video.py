0fa3f2421ede568a8f1d0b19dc0a5ddfe09ca948
Jakub Dubec <jakub.dubec@gmail.com>
2022-05-02 21:37:37 +0200

    Use default ffmpeg RTSP config (inherit from capture card) 👩🏻‍🎓

--- nug_dongle/services/video.py
+++ nug_dongle/services/video.py
@@ -11,12 +11,21 @@ class VideoService:
     def start(self, width: int, height: int, stream_name: str):
         if not self._process:
             with open(self._config['stdout'], "ab") as out, open(self._config['stderr'], "ab") as err:
+                # MJPEG
+                cmd = " ".join([
+                    'ffmpeg', '-i', self._config['device'], '-s', f'{width}x{height}', '-input_format', 'mjpeg',
+                    '-c:v', 'mjpeg', '-f', 'v4l2', '-pix_fmt', 'yuvj420p', '-vcodec', 'mjpeg', '-huffman', '0',
+
+                ])
+
+                # Default - faster (both methods sucks tbh)
+                cmd = " ".join([
+                    'ffmpeg', '-i', self._config['device'], '-s', f'{width}x{height}',
+                    '-f', 'rtsp', '-rtsp_transport', 'tcp', f"rtsp://{self._config['server']}/{stream_name}"
+                ])
+
                 self._process = Popen(
-                    " ".join([
-                        'ffmpeg', '-i', self._config['device'], '-s', f'{width}x{height}', '-input_format', 'mjpeg',
-                        '-c:v', 'mjpeg', '-f', 'v4l2', '-pix_fmt', 'yuvj420p', '-vcodec', 'mjpeg', '-huffman', '0',
-                        '-f', 'rtsp', '-rtsp_transport', 'tcp', f"rtsp://{self._config['server']}/{stream_name}"
-                    ]),
+                    cmd,
                     stdout=out,
                     stderr=err,
                     shell=True
